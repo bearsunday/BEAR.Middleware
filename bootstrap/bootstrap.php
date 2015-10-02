990ece7f0f1e59a4c1e0aeb08cde5359aaa24076
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-10-02 11:43:08 +0900

    add bootstrap for bear/skeleton app
    place this file to {app}/bootstarp/

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,10 +1,15 @@
 <?php
 
+/**
+ * @global string $context
+ */
+namespace __PACKAGE__\__VENDOR__;
+
+use BEAR\AppMeta\AppMeta;
+use BEAR\Middleware\Boot;
 use BEAR\Middleware\Handler\ResourceHandler;
-use BEAR\Middleware\Module\MiddlewareModule;
 use BEAR\Middleware\Resolver;
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Ray\Di\Injector;
 use Relay\Middleware\ExceptionHandler;
 use Relay\Middleware\ResponseSender;
 use Relay\RelayBuilder;
@@ -12,11 +17,11 @@ use Zend\Diactoros\Response;
 use Zend\Diactoros\ServerRequestFactory;
 
 loader: {
-    $loader = require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+    $loader = require dirname(__DIR__) . '/vendor/autoload.php';
     AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 }
 init: {
-    $injector = new Injector(new MiddlewareModule('BEAR\Middleware'));
+    $injector = (new Boot)->getInjector(new AppMeta(__NAMESPACE__), $context);
     $relayBuilder = new RelayBuilder(new Resolver($injector));
     $queue = [
         ResponseSender::class,
@@ -27,4 +32,4 @@ init: {
     $request = ServerRequestFactory::fromGlobals();
 }
 
-$response = $relay($request, new Response);
+$relay($request, new Response);
