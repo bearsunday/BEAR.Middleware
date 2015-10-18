ac7e6c24acd6065a5efa34a975614ef14d4eb04b
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-10-18 22:25:59 +0900

    remove bear/package dependency

---
+++ src/Exception/InvalidContextException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the BEAR.MiddleWare package
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Middleware\Exception;
+
+class InvalidContextException extends LogicException
+{
+}
